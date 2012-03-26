34eec945d125e4fbd3f8fbd16bf1f98a4566c206
Xikang Zhang <zhangxk2009@gmail.com>
2012-03-26 11:53:09 -0400

    data with different parameters collected

--- testSpaceAngle.m
+++ testSpaceAngle.m
@@ -2,9 +2,9 @@
 % Hankel Angle is utilized to classify training data
 clc;clear;close all;
 %% read data
-% [ym,labels,data]=read_binary_data();   % read data and labels
+[ym,labels,data]=read_binary_data();   % read data and labels
 % load bcidata_filter0125_htlsn10;
-% numLabels=numel(unique(labels));
+numLabels=numel(unique(labels));
 % load bcidata_0315_SRPCA_HSTLN_manually_selected.mat;
 % load bcidata_SRPCA0315_5_1000_first26data;
 
@@ -14,40 +14,59 @@ clc;clear;close all;
 % load bcidata_0322_SRPCA_7_100000_HSTLN_10_err;
 % ymR(:,eInd)=ymR_corr;
 % ymH(:,eInd)=ymH_corr;
-load bcidata_0322_SRPCA_7and5dot5_100000_err;
-load bcidata_0322_SRPCA_7and5dot5_100000_HSTLN_10;
-numLabels=numel(unique(labels));
+% load bcidata_0322_SRPCA_7and5dot5_100000_err;
+% load bcidata_0322_SRPCA_7and5dot5_100000_HSTLN_10;
+% numLabels=numel(unique(labels));
 
 % % eInd=[20 21 27 28 40 41 47 67 68 70 73 78 79 86 108 120 123 144 ...
 % %         156 157 158 164 167 190 193 195 199 205 206 212 215 258 260];
 % for i=1:size(ym,2)
 %     figure(1);
 %     plot(ym(:,i),'r');hold on;   plot(ymR(:,i),'b');plot(ymH(:,i),'g');hold off;
+%     figure(2);
+%     plot(abs(fftshift(fft(ym(:,i)))),'r');
+%     hold on; 
+%     plot(abs(fftshift(fft(ymR(:,i)))),'b');
+%     plot(abs(fftshift(fft(ymH(:,i)))),'g');
+%     hold off;
 %     i
-%     pause;
+% %     pause;
 % end
+
+%% Lowpass filter
+B=25;
+F=ones(size(ym,1),1);
+F(1+B:length(F)-B)=0;
+for i=1:size(ym,2)    
+    Y(:,i)=fft(ym(:,i)).*F;    
+    ymF(:,i)=real(ifft(Y(:,i)));    % not a good filter
+%     plot(abs(fftshift(fft(ymF(:,i)))),'b');
+end
+keyboard;
 % keyboard;
 %% SRPCA
-% % x=ym(:,1);
+% x=ym(:,1);
 % for i=1:length(eInd)
-%     tic
-%     % ymR(:,i)=SRPCA_e1_e2_clean(ym(:,i),4.44,1000,ones(size(x)));
-%     % ymR(:,i)=SRPCA_e1_e2_clean(ym(:,i),4.34,100000,ones(size(x)));
+for i=1:size(ym,2)
+    tic
+    % ymR(:,i)=SRPCA_e1_e2_clean(ym(:,i),4.44,1000,ones(size(x)));
+    ymR(:,i)=SRPCA_e1_e2_clean(ymF(:,i),5,100000,ones(size(ym,1),1));
 %     ymR(:,i)=SRPCA_e1_e2_clean(ym(:,eInd(i)),7,100000,ones(size(ym(:,eInd(i)))));
-%
-% %     y1=SRPCA_e1_e2_clean(ym(:,i),4.34,100000,ones(size(x)));
-% %     y2=SRPCA_e1_e2_clean(ym(:,i),4.34,1000,ones(size(x)));
-% %     y3=SRPCA_e1_e2_clean(ym(:,i),5,1000,ones(size(x)));
-% %     hold on;plot(ym(:,i),'r');plot(y1,'b');  plot(y2,'g');plot(y3,'k');hold off;
-%     figure(1);
-% %     plot(ym(:,i),'b');hold on;   plot(ymR(:,i),'r');hold off;
+
+%     y1=SRPCA_e1_e2_clean(ym(:,i),4.34,100000,ones(size(x)));
+%     y2=SRPCA_e1_e2_clean(ym(:,i),4.34,1000,ones(size(x)));
+%     y3=SRPCA_e1_e2_clean(ym(:,i),5,1000,ones(size(x)));
+%     hold on;plot(ym(:,i),'r');plot(y1,'b');  plot(y2,'g');plot(y3,'k');hold off;
+    figure(1);
+    plot(ym(:,i),'r');hold on;   plot(ymF(:,i),'k'); plot(ymR(:,i),'b');hold off;
 %     plot(ym(:,eInd(i)),'b');hold on;   plot(ymR(:,i),'r');hold off;
-%     toc
-%     fprintf('processing %d th data\n',i);
-% end
+    toc
+    fprintf('processing %d th data\n',i);
+end
 % errlabels=labels(eInd);
 % save bcidata_0322_SRPCA_7_100000_errData ymR errlabels;
 % keyboard;
+save bcidata_0325_lowpass_25Hz_SRPCA_5_100000 ymR labels;
 % % ym = ymR;
 
 % ymR(:,1)=SRPCA_e1_e2_clean(ym(:,1),4.34,100000,ones(size(x)));
@@ -57,16 +76,18 @@ numLabels=numel(unique(labels));
 
 % load bcidata_SRPCA0315_5_1000_first26data.mat
 %% denoise, order reduction
-% for i=1:size(ymR,2)
-% [yph,eta,x] = hstln_mo(ymR(:,i)',10);
-% hold on;
+for i=1:size(ymR,2)
+[yph,eta,x] = hstln_mo(ymR(:,i)',10);
+hold on;
 % plot(ym(:,eInd(i)),'r');
-% hold off;
-% yph=yph';
-% % keyboard;
-% ymH(:,i)=yph;
-% end
-% save bcidata_0322_SRPCA_7_100000_HSTLN_10_err ymH errlabels;
+plot(ym(:,i),'r');
+plot(ymF(:,i),'k');
+hold off;
+yph=yph';
+% keyboard;
+ymH(:,i)=yph;
+end
+save bcidata_0325_lowpass_25_SRPCA_5_100000_HSTLN_10 ymH labels;
 % keyboard;
 % %%
 % % yH1 = hstln_mo(ymR(:,1)',10)';
@@ -146,7 +167,7 @@ fprintf('data acquired\n');
 % % end
 % keyboard;
 
-ym=ymR;
+ym=ymH;
 
 
 
@@ -190,7 +211,8 @@ for Hsize=HsizeRange
             %fprintf('computing label of %d th test data of all %d \n',
             %             i,size(Utest,3));
             Ut=Utest(:,:,i);
-            parfor j=1:size(Utrain,3)
+            L=size(Utrain,3);
+            parfor j=1:L
                 angles(j)=subspace(Ut,Utrain(:,:,j));
             end
             all_lb=[sum(angles(trainLabels==1)) sum(angles(trainLabels==2))...
@@ -212,7 +234,7 @@ for Hsize=HsizeRange
     P(:,:,HCount)=precisionMat;
     HCount=HCount+1;
 end
-save bcidata_0322_PrecisionMat P H U order HsizeRange?
+save bcidata_0325_L25_R5_H10_PrecisionMat P H U order HsizeRange;
 
 % for ind=1:266
 % ytest=data(ind+16662:ind+16662+266-1,2);
