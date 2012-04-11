2703de5c0f59147f2d20e3cf5b9e16c16ddccbde
Xikang Zhang <zhangxk2009@gmail.com>
2012-04-11 16:09:28 -0400

    gcc works better but training and testing not seperated yet

--- testDCC.m
+++ testDCC.m
@@ -7,31 +7,51 @@ numLabels=numel(unique(labels));
 [~,ym]=sortdata(ym,labels);
 [~,labels]=sortdata(labels,labels);
 
-hsize=133;
-H=zeros(hsize,size(ym,1)-hsize+1,size(ym,2));
-for i=1:size(ym,2)
-    H(:,:,i)=hankel(ym(1:hsize,i),ym(hsize:end,i));
-end
-
-pca_dim=60;
-P=[];
-opt=1;
-for i=1:size(ym,2)
-    [P1,D1] = Eigen_Decompose(H(:,:,i),opt);
-    P=[P P1(:,1:pca_dim)];
-end
-
-Pm=P;
+pca_dim=1;
+Pm=ym;
 n_class=4;
 n_set=65;
-red_dim=100;
+red_dim=200;
 T = Learn_DCC(Pm,n_class,n_set,pca_dim,red_dim);
 
-ym=[];
-Q=T'*P;
-for i=1:(size(Q,2)/pca_dim)
-    temp=Q(:,(i-1)*pca_dim+1:i*pca_dim);
-    Y(:,:,i)=temp;
-    ym(:,i)=temp(:,1);
+ym=T'*ym;
+% save dataY ym labels;
+
+%% Cross Validation
+cRange=1;
+for kk=1:length(cRange)
+crossIdx=crossvalind('Kfold',size(ym,2),10);
+GRlabels=[];
+TTlabels=[];
+for k=1:10
+    % Training
+    ytrain=ym(:,crossIdx~=k);
+    trainLabels=labels(:,crossIdx~=k);
+    model = svmtrain(trainLabels', ytrain',['-s 1 -t 1 -d 1 -c ' num2str(cRange(kk))]);
+    
+    % Testing
+    ytest=ym(:,crossIdx==k);
+    testLabels=labels(crossIdx==k);
+    [predicted_label, accuracy, decision_values] = svmpredict(testLabels', ytest', model);
+%     pause;
+    lb=predicted_label';
+%     for i=1:size(ytest,2)
+%         d_all=sum((repmat(ytest(:,i),1,numLabels)-template).^2);
+%         [Y,I]=min(d_all);
+%         lb(i)=I;
+%     end
+    GRlabels=[GRlabels testLabels];
+    TTlabels=[TTlabels lb];
 end
-save dataY ym labels;
+
+%% Display results
+[GRlabels;TTlabels]
+precisionMat=zeros(numLabels);
+for j=1:numLabels
+    for i=1:numLabels
+        precisionMat(i,j)=nnz(TTlabels(GRlabels==j)==i)/nnz(GRlabels==j);
+    end
+end
+precisionMat
+z(kk)=trace(precisionMat)/4
+end
